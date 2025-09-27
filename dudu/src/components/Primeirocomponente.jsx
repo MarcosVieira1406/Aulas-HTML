0257d8f17032a6a20713cb86b50290df9ca6c594
Marcos Eduardo Surita Vieira <mesuritavieira@gmail.com>
2025-09-26 21:47:38 -0300

    src

---
+++ dudu/src/components/Primeirocomponente.jsx
@@ -0,0 +1,10 @@
+import '../App.css'
+
+function Primeirocomponente(){
+    return(
+        <div>
+            <h2>Meu primeiro componente</h2>
+        </div>
+    )
+}
+export default Primeirocomponente
