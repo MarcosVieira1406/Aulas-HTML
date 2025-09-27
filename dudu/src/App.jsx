131dcf276ddcd383c2e5635fa1c46d3ea96b5079
Marcos Eduardo Surita Vieira <mesuritavieira@gmail.com>
2025-09-26 21:30:38 -0300

    Html

--- dudu/src/App.jsx
+++ dudu/src/App.jsx
@@ -2,33 +2,57 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import peterbot from './assets/peterbot.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 id="myheader">Oficina React</h1>
+    <h2>Seminário</h2>
+    <h3>Dia 2 - 26/09/2025</h3>
+    <a 
+    href= "https://www.google.com/" id="link">Google</a>
+
+    <ul class="lista">
+        <li>Segunda</li>
+        <li>Terça</li>
+        <li>Quarta</li>
+        <li>Quinta</li>
+        <li>Sexta</li>
+        <li>Sábado</li>
+        <li>Domingo</li>
+    </ul>
+
+    <ol class="lista">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+    </ol>
+    
+    <table class="tabelas">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Profissão</th>
+        </tr>
+        <tr>
+            <td>01</td>
+            <td>Marcos</td>
+            <td>Programador</td>
+        </tr>
+        <tr>
+            <td>02</td>
+            <td>Alexandra</td>
+            <td>Contabilidade</td>
+        </tr>
+    </table>
+    <p>Carro</p>
+    <img src='/carro.jpg' alt='carro'></img>
+    <p>Peterbot</p>
+    <img src={peterbot} alt="outra imagem"></img>
+    </div>
   )
 }
 
